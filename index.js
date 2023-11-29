864dba2d58dfe52bdc14f2c8a5bc52d4e877c41f
Gerulis Donatas <okonecnikovaite.monika@gmail.com>
2023-11-29 13:36:36 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+//1 uzd
+// let kiekSkaiciu = parseInt(prompt("kiek skaiciu: "));
+// for(let i = 0; i <= kiekSkaiciu; i++){
+//     let kokieSkaiciai = parseInt(prompt("Iveskite skaiciu: "));
+//     if(kokieSkaiciai < 0){
+//     alert(`pirmas neigiamas skaicius buvo ${kokieSkaiciai}`)
+//     break;
+//     }
+// }
+//2 uzd
+// for(let i = 1; i <= 100; i++){
+//     if(i % 3 === 0)
+//         continue;
+//         console.log(i)
+// }
+//3
+// for(let i = 2; i <= 12; i++){
+//     kubas = i**3
+//     console.log(`Skaiciaus ${i} pakeltas kubu yra ${kubas}`);
+// }
+//4
+const persons = {
+    jonas: 30,
+    petras: 26,
+    ona:45,
+    maryte:36,
+    antanas:77,
+    "teta Zita":86,
+};
